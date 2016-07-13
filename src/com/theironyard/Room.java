e232d45bdb996f64bff52a9523ee029894273f4e
Ben <bhturne@g.clemson.edu>
2016-07-13 11:25:22 -0400

    generates full maze

--- src/com/theironyard/Room.java
+++ src/com/theironyard/Room.java
@@ -6,10 +6,46 @@ package com.theironyard;
 public class Room{
     int row;
     int col;
+    boolean hasBottom = true;
+    boolean hasRight = true;
+    boolean wasVisited = false;
+
+
+    public Room(int row, int col, boolean hasBottom, boolean hasRight, boolean wasVisited) {
+        this.row = row;
+        this.col = col;
+        this.hasBottom = hasBottom;
+        this.hasRight = hasRight;
+        this.wasVisited = wasVisited;
+    }
 
     public Room(int row, int col) {
         this.row = row;
         this.col = col;
+    }
+
+    public boolean isWasVisited() {
+        return wasVisited;
+    }
+
+    public void setWasVisited(boolean wasVisited) {
+        this.wasVisited = wasVisited;
+    }
+
+    public boolean isHasBottom() {
+        return hasBottom;
+    }
+
+    public void setHasBottom(boolean hasBottom) {
+        this.hasBottom = hasBottom;
+    }
+
+    public boolean isHasRight() {
+        return hasRight;
+    }
+
+    public void setHasRight(boolean hasRight) {
+        this.hasRight = hasRight;
     }
 
     public int getRow() {
